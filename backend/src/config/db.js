9c2c44e27d87426d8d000f82770cbdba048cc134
DIOGODTF <dtf.diogo@gmail.com>
2022-03-29 06:51:33 -0300

    Aula 11

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -9,3 +9,14 @@ const db = mysql.createPool({
 });
 
 export default db;
+
+/** se tiver problema pra acessar o mysql
+ * ALLTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '12345';
+ * flush privileges;
+ *
+ * 
+ * PRA RESOLVER PROBLEMAS DE RESTRICAO NO POWERSHEEL
+ * Get-ExecutionPolicy
+ * se for restrito, é so trocar
+ * Set-ExecutionPolicy RemoteSigned
+ */
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import db from './config/db.js';
+import controllerUsuarios from './controllers/controller.usuarios.js';
+import controllerMercados from './controllers/controller.mercados.js';
+import controllerProdutos from './controllers/controller.produtos.js';
+import controllerPedidos from './controllers/controller.pedidos.js';
 
 const app = express();
 
@@ -9,96 +12,10 @@ app.use(cors());
 //middlare do json
 app.use(express.json());
 
-/* 
-   GET - retorna dados
-   POST - Cadastrar dados
-   PUT - Editar dados
-   PATCH - Editar dados
-   DELETE - excluir dados
-*/
-app.get('/status', function(request, response ){
-    let d = new Date; 
-    d = `${('00'+d.getDate()).slice(-2)}/${('00'+d.getMonth()).slice(-2)}/${('0000'+d.getFullYear()).slice(-2)}`;
-    return response           
-           .status(200)
-           .send(`{"error":"","message":"Server started Ok!","date":"${d}"}`);           
-})
-
-app.get('/usuarios', function( request, response){
-    let ssql = 'select * from usuario';
-    db.query(ssql, function( err, result){
-        if (err) {
-            return response.status(500).send(err);
-        } else {
-            /*devolve um array de json
-            */
-            return response.status(200).json(result);
-        }
-    });    
-} );
-
-app.get('/usuarios/:id', function( request, response){
-    let ssql = 'select * from usuario where id_usuario = ?';
-    db.query(ssql,[request.params.id], function( err, result){
-        if (err) {
-            return response.status(500).send(err);
-        } else {
-            /*devolve um unico objeto json 
-            */
-            return response.status(result.length > 0 ? 200 : 404).json(result[0]); 
-        }
-    });    
-} );
-
-app.post('/usuarios/login', function (request, response){
-    let ssql ="select id_usuario, nome, email, endereco, bairro, cidade, uf, cep, ";
-        ssql+="date_format(dt_cadastro,'%d/%m/%Y %H:%i:%s' ) as dt_cadastro ";
-        ssql+="from usuario where email = ? and senha = ?";
-
-    db.query(ssql, [request.body.email, request.body.senha], function(err, result){
-        if (err) {
-            return response.status(500).send(err);
-        } else {
-            return response.status(result.length>0 ? 200 : 401).json(result[0]);
-        }   
-    });
-});
-
-app.post('/usuarios/cadastro',function(request, response){
-
-});
-
-app.put('/usuarios/:id',function(request, response){    
-    let ssql = "update usuario set nome = ?, endereco = ?, bairro = ?, cidade = ?, uf = ?, cep = ? "
-        ssql+= "where id_usuario = "+request.params.id;
-    const body = request.body;    
-    db.query(ssql,[body.nome,
-                   body.endereco, 
-                   body.bairro, 
-                   body.cidade, 
-                   body.uf, 
-                   body.cep],
-                   function(err, result){
-                        if (err) {
-                            return response.status(500).send(err);
-                        } else {
-                            return response.status(result.length>0?200:401).json(result[0]);
-                        }
-                   }
-            );    
-});
-
-app.delete('/usuarios/:id',function(request, response){
-    let ssql = 'delete from usuario where id_usuario = ?';
-    db.query(ssql,[request.params.id], function(err, result){
-        if (err) {
-            return response.status(500).send(err);
-        } else {
-            return response.status(result.length>0 ? 200 : 401).json(result[0]);
-        }
-    });
-});
-
+app.use( controllerUsuarios );
+app.use( controllerMercados );
+app.use( controllerProdutos );
+app.use( controllerPedidos );
 /*
 //uri params (/clientes/10)
 app.get('/clientes/:id', function( request, response){
@@ -121,4 +38,5 @@ app.post('/clientes', function( request, response){
 app.listen(3000, function() {
    console.log('..:: servidor no ar ::..');
 })
+
 
